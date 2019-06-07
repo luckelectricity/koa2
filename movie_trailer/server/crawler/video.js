003fe5db807b9b896258f3f6ec142e8aaf8b575b
luckelectricity <luckelectricity@outlook.com>
2019-06-07 22:54:05 +0800

    feat 爬取video连接和背景图

---
+++ movie_trailer/server/crawler/video.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer');
+const base = 'https://movie.douban.com/subject/';
+const doubanId = '26891256';
+const videoUrl = 'https://movie.douban.com/trailer/246013/#content';
+const sleep = time =>
+  new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+(async () => {
+  console.log('开始爬取目标页面');
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  });
+  const page = await browser.newPage();
+
+  await page.goto(base + doubanId, {
+    waitUntil: 'networkidle2'
+  });
+
+  await sleep(1000);
+
+  const result = await page.evaluate(() => {
+    var $ = window.$;
+    var item = $('.related-pic-video');
+    if (item && item.length > 0) {
+      var link = item.attr('href');
+      var cover = item
+        .attr('style')
+        .replace('background-image:url(', '')
+        .replace('?)', '');
+      return {
+        link,
+        cover
+      };
+    }
+    return {}
+  });
+  let video;
+  if(result.link){
+    await page.goto(result.link, {
+      waitUntil: 'networkidle2'
+    })
+    console.log('打开之前')
+    await sleep(2000);
+    video = await page.evaluate(() => {
+      var $ = window.$;
+      var item = $('source')
+      if(item && item.length > 0){
+        return item.attr('src')
+      }
+      return ''
+    })
+  }
+  const data = {
+    video,
+    doubanId,
+    cover: result.cover
+  }
+  browser.close();
+  // console.log(result);
+  process.send(data);
+  process.exit(0);
+})();
---
+++ movie_trailer/server/tasks/trailer.js
@@ -0,0 +1,23 @@
+const cp = require('child_process');
+const { resolve } = require('path');
+
+(async () => {
+  const script = resolve(__dirname, '../crawler/video.js');
+  const child = cp.fork(script, []);
+  let inworked = false;
+
+  child.on('error', err => {
+    if (inworked) return;
+    inworked = true;
+  });
+
+  child.on('exit', code => {
+    if (inworked) return;
+    inworked = true;
+    let err = code === 0 ? null : new Error('exit code ' + code);
+    console.log(err);
+  });
+  child.on('message', data => {
+    console.log(data);
+  });
+})();
